d7d150cef8a97593ddcf6ff401d65499b0fdeb6a
Cuciuc Maxim <maxim.cuciuc123@gmail.com>
2025-09-17 16:53:04 +0300

    individual preview finished

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -7,6 +7,8 @@ import IconButton from "./icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "./currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 
 interface ProductCard {
@@ -17,12 +19,18 @@ const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
     const router = useRouter();
+    const previewModal = usePreviewModal()
     
     const handleClick = () => {
         router.push(`/product/${data?.id}`);
     }
 
 
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation()
+        event.preventDefault()
+        previewModal.onOpen(data)
+    }
     return (
         <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
            <div className="aspect-square rounded-xl bg-gray-100 relative">
@@ -36,7 +44,7 @@ const ProductCard: React.FC<ProductCard> = ({
              <div className="opacity-0 group-hover:opacity-100 transition absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-10">
                 <div className="flex gap-x-6 justify-center">
                     <IconButton
-                    onClick={() => {}}
+                    onClick={onPreview}
                     icon={<Expand size ={20} className="text-gray-600" />}
                     />
                      <IconButton
